774b4088d02562479cc5bdcc29630dfd0b67ab3f
Wister <e@wister.mx>
2020-03-27 12:08:49 -0600

    separating functions

--- main.go
+++ main.go
@@ -24,25 +24,3 @@ func orchestrator(){
 	fmt.Println(hashes)
 	fmt.Println(text)
 }
-
-func maindisplay(){
-
-	fmt.Println("////////////////////////////")
-	fmt.Println("////Running first",version,"////")
-	fmt.Println("////////////////////////////")
-	fmt.Println("////	Blackvault	////")
-	fmt.Println("////	 Wister		////")
-	fmt.Println("////	",date,"	////")
-	fmt.Println("////////////////////////////")
-	fmt.Println("////////////////////////////")
-}
-
-//func makehash(tohash string ) string {
-
-//	h := sha1.New()
-//	h.Write([]byte(tohash))
-//	sha1_hash := hex.EncodeToString(h.Sum(nil))
-
-//	fmt.Println(tohash, sha1_hash)
-//	return sha1_hash
-//}
---
+++ screenmanager.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func maindisplay(){
+
+	fmt.Println("////////////////////////////")
+	fmt.Println("////Running first",version,"////")
+	fmt.Println("////////////////////////////")
+	fmt.Println("////	Blackvault	////")
+	fmt.Println("////	 Wister		////")
+	fmt.Println("////	",date,"	////")
+	fmt.Println("////////////////////////////")
+	fmt.Println("////////////////////////////")
+}
