e97fb4751c5dd6d292ff59b7eede5b3cf24afeb1
Wister <e@wister.mx>
2020-03-27 21:16:16 -0600

    first contact

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
 var (
 	version = "Alpha"
 	date = "2020/01"
@@ -13,14 +8,10 @@ var (
 
 func main() {
 	orchestrator()
+
 }
 
 func orchestrator(){
 	maindisplay()
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	var text, _ = reader.ReadString('\n')
-	var hashes = makehash(text)
-	fmt.Println(hashes)
-	fmt.Println(text)
+	capturerdisplay()
 }
--- screenmanager.go
+++ screenmanager.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func maindisplay(){
 
@@ -11,5 +16,42 @@ func maindisplay(){
 	fmt.Println("////	 Wister		////")
 	fmt.Println("////	",date,"	////")
 	fmt.Println("////////////////////////////")
+	fmt.Println("////	 1 - to encrypt	////")
+	fmt.Println("////	 2 - to decrypt ////")
 	fmt.Println("////////////////////////////")
 }
+func capturerdisplay() {
+
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter option: ")
+	var text, _ = reader.ReadString('\n')
+	fmt.Println("Enter the key")
+	var keytext, _ = reader.ReadString('\n')
+	fmt.Println("Enter the secret to hash")
+	var plaintexttext, _ = reader.ReadString('\n')
+	key := []byte(keytext) // 32 bytes
+	plaintext := []byte(plaintexttext)
+	if  text == "1" {
+		callEncrypted(key,plaintext)
+	}else{
+		callDecrypt(key,plaintext)
+	}
+}
+
+func callEncrypted(keytext []byte , plaintexttext []byte ){
+	ciphertext, err := encrypt(keytext, plaintexttext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", ciphertext)
+}
+func callDecrypt(keytext []byte , plaintexttext []byte) {
+
+	result, err := decrypt(keytext, plaintexttext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", result)
+}
+
