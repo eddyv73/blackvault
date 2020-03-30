e39307ead1c9dcf399ee773d0f0d8e516f50469f
Wister <e@wister.mx>
2020-03-29 22:48:10 -0600

    refactor

--- hasher.go
+++ hasher.go
@@ -2,110 +2,45 @@ package main
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
+	"encoding/hex"
 	"fmt"
-	"io"
-	"log"
 )
 
 func makehash(text string, keytext string ,plaintexttext string ) bool {
 	fmt.Println("hasher running")
 	var greenlight = false
 	if  clearinput(text) == "1" {
-		callEncrypted(keytext,clearinput(plaintexttext))
+		e := Encrypt([]byte(keytext),clearinput(plaintexttext))
+		fmt.Printf("%s\n", e)
+
 		greenlight = true
 	}else{
-		callDecrypt(keytext,clearinput(plaintexttext))
+		Decrypt([]byte(keytext),clearinput(plaintexttext))
 		greenlight = true
 	}
 	return greenlight
 }
-
-func callEncrypted(keytext string , plaintexttext string ){
-	ciphertext, err := encryptString(keytext,plaintexttext)
+func Encrypt(key []byte, plaintext string) string {
+	c, err := aes.NewCipher(key)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Errorf("NewCipher(%d bytes) = %s", len(key), err)
+		panic(err)
 	}
-	fmt.Printf("%s\n", ciphertext)
+	out := make([]byte, len(plaintext))
+	c.Encrypt(out, []byte(plaintext))
+
+	return hex.EncodeToString(out)
 }
-func callDecrypt(keytext string , plaintexttext string) {
-
-	result,err := decryptString(plaintexttext,keytext)
+func Decrypt(key []byte, ct string) {
+	ciphertext, _ := hex.DecodeString(ct)
+	c, err := aes.NewCipher(key)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Errorf("NewCipher(%d bytes) = %s", len(key), err)
+		panic(err)
 	}
-	fmt.Printf("%s\n", result)
-	message := base64.StdEncoding.EncodeToString([]byte(result))
-	fmt.Printf("%s\n", message)
-}
-func hashTo32Bytes(input string) []byte {
-
-	data := sha256.Sum256([]byte(input))
-	return data[0:]
-
-}
-func decryptString(cryptoText string, keyString string) (plainTextString string, err error) {
-
-	encrypted, err := base64.URLEncoding.DecodeString(cryptoText)
-	if err != nil {
-		return "", err
-	}
-	if len(encrypted) < aes.BlockSize {
-		return "", fmt.Errorf("cipherText too short. It decodes to %v bytes but the minimum length is 16", len(encrypted))
-	}
-	decrypted, err := decryptAES(hashTo32Bytes(keyString), encrypted)
-	if err != nil {
-		return "", err
-	}
-	s := string(decrypted)
-	return s, nil
-}
-func decryptAES(key, data []byte) ([]byte, error) {
-	// split the input up in to the IV seed and then the actual encrypted data.
-	iv := data[:aes.BlockSize]
-	data = data[aes.BlockSize:]
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	stream.XORKeyStream(data, data)
-	return data, nil
-}
-func encryptString(plainText string, keyString string) (cipherTextString string, err error) {
-
-	key := hashTo32Bytes(keyString)
-	encrypted, err := encryptAES(key, []byte(plainText))
-	if err != nil {
-		return "", err
-	}
-	return base64.URLEncoding.EncodeToString(encrypted), nil
-}
-func encryptAES(key, data []byte) ([]byte, error) {
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	// create two 'windows' in to the output slice.
-	output := make([]byte, aes.BlockSize+len(data))
-	iv := output[:aes.BlockSize]
-	encrypted := output[aes.BlockSize:]
-
-	// populate the IV slice with random data.
-	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	// note that encrypted is still a window in to the output slice
-	stream.XORKeyStream(encrypted, data)
-	return output, nil
+	plain := make([]byte, len(ciphertext))
+	c.Decrypt(plain, ciphertext)
+	s := string(plain[:])
+	fmt.Printf("AES Decrypyed Text:  %s\n", s)
 }
 
