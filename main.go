88e433661c180df85b939783b547aea5d28bcf21
Wister <e@wister.mx>
2020-04-01 00:50:08 -0600

    Run with Arguments

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 var (
 	version = "Alpha"
 	date = "2020/01"
 
+
 )
 
 func main() {
-	orchestrator()
+	argsWithProg := os.Args
+	orchestrator(argsWithProg)
 }
 
-func orchestrator(){
+func orchestrator(optionrunning []string) {
+	optiona := contains(optionrunning,"-a")
+	fmt.Println(optiona)
 	maindisplay()
 	capturerdisplay()
 	GoodBye()
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
 }
